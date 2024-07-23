300cca6b363d1bedcc18d76fcc828427b4ca655e
ShadiSoufan44 <shadi_soufan@brown.edu>
2024-07-22 22:40:06 -0400

    Finished week 7 lab

--- src/component/TodoData.js
+++ src/component/TodoData.js
@@ -1,23 +1,24 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-
 
 const ShowTodos = () => {
   //Begin Here
   const [todos, setTodos] = useState([]);
-  
-  useEffect( () => { 
-      async function fetchData() {
-          try {
-              const res = await Axios.get('http://localhost:8080/items'); 
-              setTodos(JSON.stringify(res.data));
-              console.log(JSON.stringify(res.data));
-          } catch (err) {
-              console.log(err);
-          }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await Axios.get("http://localhost:8080/items", {
+          withCredentials: true,
+        });
+        setTodos(JSON.stringify(res.data));
+        console.log(JSON.stringify(res.data));
+      } catch (err) {
+        console.log(err);
       }
-      fetchData();
+    }
+    fetchData();
   }, []);
-  return <div>{todos}</div>
-}
+  return <div>{todos}</div>;
+};
 export default ShowTodos;
